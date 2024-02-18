923a9627bb32dad99a6c039a10741b92a9e3dd20
Alejandro Gil <ZeroCool940711@users.noreply.github.com>
2024-02-17 22:31:20 -0700

    Added typing and docstrings to the utils functions.

--- src/sygil_authy/utils.py
+++ src/sygil_authy/utils.py
@@ -2,6 +2,7 @@ import ctypes
 import os
 
 from whoosh import index
+from whoosh.fields import Schema
 
 import sygil_authy
 
@@ -16,7 +17,7 @@ APP_ID = f"sygil_authy.Sygil-Dev.version.{module_name.__version__}"
 ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(APP_ID)
 
 
-def set_icon(icon_path="src/sygil_authy/assets/icon.ico"):
+def set_icon(icon_path: str = "src/sygil_authy/assets/icon.ico") -> None:
     """
     Sets the icon of the foreground window.
 
@@ -54,7 +55,7 @@ def set_icon(icon_path="src/sygil_authy/assets/icon.ico"):
     user32.SendMessageW(hwnd, WM_SETICON, ICON_BIG, hicon)
 
 
-def first_run():
+def first_run() -> bool:
     """
     Checks if the index 'options' exists in the 'db' database. If the index does not exist,
     it sets the default configuration by calling the 'set_default_config()' function and
@@ -71,28 +72,109 @@ def first_run():
         return False
 
 
-def create_index(schema, indexname):
-    # Using the convenience functions
+def create_index(schema: Schema, indexname: str) -> index.Index:
+    """
+    Create an index using the specified schema and index name.
+
+    Args:
+        schema (Schema): The schema to be used for the index.
+        indexname (str): The name of the index.
+
+    Returns:
+        Index: The created index.
+
+    Raises:
+        OSError: If there is an error creating the index.
+
+    Example:
+        >>> schema = Schema(id=ID(stored=True), name=TEXT(stored=True))
+        >>> index = create_index(schema, "my_index")
+    """
     ix = index.create_in("db", schema=schema, indexname=indexname)
     return ix
 
 
-def open_index(dirname, indexname, schema):
-    # Using the convenience functions
+def open_index(dirname: str, indexname: str, schema: Schema) -> index.Index:
+    """
+    Opens an index in the specified directory with the given name and schema.
+
+    Args:
+        dirname (str): The directory path where the index is located.
+        indexname (str): The name of the index to be opened.
+        schema (Schema): The schema object defining the index structure.
+
+    Returns:
+        Index: The opened index object.
+
+    Raises:
+        OSError: If the index directory or file cannot be accessed.
+        index.EmptyIndexError: If the index is empty or does not exist.
+        index.IndexVersionError: If the index version is incompatible.
+
+    Example:
+        >>> schema = Schema(title=TEXT(stored=True), content=TEXT)
+        >>> ix = open_index('/path/to/index', 'my_index', schema)
+    """
     ix = index.open_dir(dirname=dirname, indexname=indexname, schema=schema)
     return ix
 
 
-def check_if_index_exists(dirname, indexname):
+def check_if_index_exists(dirname: str, indexname: str) -> bool:
+    """
+    Check if an index exists in a directory.
+
+    Args:
+        dirname (str): The directory path.
+        indexname (str): The name of the index.
+
+    Returns:
+        bool: True if the index exists, False otherwise.
+    """
     return index.exists_in(dirname, indexname=indexname)
 
 
-def make_account_index():
+def make_account_index() -> index.Index:
+    """
+    Creates and returns an instance of the index.Index class for managing accounts.
+
+    This function initializes an index for storing and retrieving account objects.
+    It uses the create_index function from the index module to create the index.
+
+    Returns:
+        index.Index: An instance of the index.Index class.
+    """
     ix = create_index(Account(), "accounts")
     return ix
 
 
-def add_account(account):
+def add_account(account: dict) -> None:
+    """
+    Add an account to the database index.
+
+    This function adds a new account to the database index. If the index already exists,
+    it opens the existing index. Otherwise, it creates a new index.
+
+    Parameters:
+    - account (dict): A dictionary representing the account to be added. The dictionary
+      should contain the necessary fields for the account.
+
+    Returns:
+    - None
+
+    Example usage:
+    ```
+    account = {
+        "username": "john_doe",
+        "email": "john@example.com",
+        "website": "example.com",
+    }
+    add_account(account)
+    ```
+
+    Note:
+        - The function assumes that the necessary imports and index-related functions are available.
+        - The function assumes that the index schema is defined as `Account`.
+    """
     if check_if_index_exists("db", "accounts"):
         ix = index.open_dir("db", indexname="accounts", schema=Account)
     else:
@@ -101,6 +183,56 @@ def add_account(account):
     writer = ix.writer()
     writer.add_document(**account)
     writer.commit(merge=True, optimize=True)
+
+
+def delete_account(account_id: int) -> None:
+    """
+    Delete an account from the database.
+
+    Args:
+        account_id (int): The ID of the account to be deleted.
+
+    Returns:
+        None
+
+    Raises:
+        None
+
+    Notes:
+        This function deletes an account from the database by performing the following steps:
+        1. Open the index directory named "db" with the schema defined as Account.
+        2. Create a writer object to perform write operations on the index.
+        3. Delete the account with the specified account_id by using the "delete_by_term" method of the writer.
+        4. Commit the changes made by the writer to the index, merging and optimizing the index.
+
+    Example:
+        delete_account(123)  # Delete the account with ID 123 from the database.
+    """
+    ix = index.open_dir("db", indexname="accounts", schema=Account)
+    writer = ix.writer()
+    writer.delete_by_term("id", account_id)
+    writer.commit(merge=True, optimize=True)
+
+
+def update_account(account: dict) -> None:
+    ix = index.open_dir("db", indexname="accounts", schema=Account)
+    writer = ix.writer()
+    writer.update_document(**account)
+    writer.commit(merge=True, optimize=True)
+
+
+def search_accounts(query, last_version=True):
+    ix = index.open_dir("db", indexname="accounts", schema=Account)
+
+    with ix.searcher() as searcher:
+        results = searcher.documents(**query)
+        if last_version:
+            # return last version only
+            for result in results:
+                return result
+        else:
+            # return all versions/backups
+            return list(results)
 
 
 def set_default_config(reset=False):
